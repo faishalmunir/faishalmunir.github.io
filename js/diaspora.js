57c4d3521699bc299f4f1564f8a39dab185d8c81
Zyca <ragnanear@gmail.com>
2020-11-25 17:07:18 +0700

    Site updated: 2020-11-25 17:07:16

--- js/diaspora.js
+++ js/diaspora.js
@@ -617,7 +617,7 @@ $(function() {
 					gitalk.render('gitalk-container')
 					Diaspora.loaded();
 				}else{
-					$('#gitalk-container').html("评论已关闭");
+					$('#gitalk-container').html("Comments");
 				}
                 return false;
                 break;
